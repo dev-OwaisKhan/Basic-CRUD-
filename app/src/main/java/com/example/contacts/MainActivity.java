67d618ea6fb233d061430189c631dc5a9cd13a6d
dev-OwaisKhan <dev.owaiskhan@gmail.com>
2022-09-03 12:44:42 +0530

    added handler

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -3,15 +3,41 @@ package com.example.contacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import data.DataHandler;
+import modle.Contact;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name, phone;
+    String name_text, phone_text;
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         DataHandler handler = new DataHandler(MainActivity.this);
+        Contact obj = new Contact();
+
+        name = findViewById(R.id.name);
+        phone = findViewById(R.id.phone);
+        add = findViewById(R.id.add_button);
+
+        name_text = name.getText().toString();
+        phone_text = phone.getText().toString();
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                obj.setName(name_text);
+                obj.setPhone(phone_text);
+                handler.addcontact(obj);
+            }
+        });
     }
 }
--- app/src/main/java/data/DataHandler.java
+++ app/src/main/java/data/DataHandler.java
@@ -1,9 +1,15 @@
 package data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import com.example.contacts.MainActivity;
+import com.example.contacts.R;
+
+import modle.Contact;
 import parameters.Param;
 
 public class DataHandler extends SQLiteOpenHelper {
@@ -15,16 +21,30 @@ public class DataHandler extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String create  = "CREATE TABLE "+Param.TABLE_NAME+" (" + Param.KEY_ID+ "INTEGER PRIMARY KEY, B6"
-                + Param.KEY_NAME + "TEXT, "+ Param.KEY_PHONE + "TEXT )";
-        sqLiteDatabase.execSQL(create);
+    public void onCreate(SQLiteDatabase db) {
+        String create  = "CREATE TABLE " + Param.TABLE_NAME + "(" + Param.KEY_ID+ "INTEGER PRIMARY KEY, "
+                + Param.KEY_NAME + "TEXT, "+ Param.KEY_PHONE + "TEXT" + ")";
+        Log.d("Owais","run");
+        db.execSQL(create);
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
 
+    }
+
+    public void addcontact(Contact contact)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Param.KEY_NAME,contact.getName());
+        values.put(Param.KEY_PHONE,contact.getPhone());
+
+        db.insert(Param.TABLE_NAME,null,values);
+        Log.d("Owais","INserted");
+        db.close();
     }
 
 }
